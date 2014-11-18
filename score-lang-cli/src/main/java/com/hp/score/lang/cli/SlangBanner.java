369ca0f1b12daa71438e771c9667bd1a4f81a04d
Avi Moradi <avi.moradi@hp.com>
2014-11-18 19:03:29 +0200

    spring-shell upgrade

--- score-lang-cli/src/main/java/com/hp/score/lang/cli/SlangBanner.java
+++ score-lang-cli/src/main/java/com/hp/score/lang/cli/SlangBanner.java
@@ -1,70 +1,53 @@
-package com.hp.score.lang.cli;
-
 /*
- * Licensed to Hewlett-Packard Development Company, L.P. under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
-*/
-
-
-import org.apache.commons.io.IOUtils;
-import org.springframework.shell.plugin.BannerProvider;
-import org.springframework.stereotype.Component;
+ * Licensed to Hewlett-Packard Development Company, L.P. under one or more contributor license agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
+package com.hp.score.lang.cli;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.annotation.Order;
+import org.springframework.shell.plugin.BannerProvider;
+import org.springframework.stereotype.Component;
+
 /**
- * Date: 11/7/2014
- *
  * @author lesant
+ * @since 11/07/2014
+ * @version $Id$
  */
 @Component
-public class SlangBanner implements BannerProvider {
+@Order(Integer.MIN_VALUE)
+public class SlangBanner extends SlangNamedProvider implements BannerProvider {
 
-    private static final String assistance = "Welcome to Slang. For assistance type help.";
+	private static final String BANNER = "slangBanner.txt";
+	private static final String ASSISTANCE = "Welcome to Slang. For assistance type help.";
 
-    private static final String banner = "slangBanner.txt";
+	@Override
+	public String getBanner() {
+		StringBuilder sb = new StringBuilder();
+		try (InputStream in = ClassLoader.getSystemResourceAsStream(BANNER)) {
+			sb.append(IOUtils.toString(in));
+		} catch(IOException e) {
+			sb.append("Slang");
+		}
+		sb.append(System.lineSeparator());
+		sb.append(SlangCLI.getVersion());
+		return sb.toString();
+	}
 
-    @Override
-    public String getBanner() {
-        StringBuilder sb = new StringBuilder();
-        try (InputStream in = ClassLoader.getSystemResourceAsStream(banner)) {
-            sb.append(IOUtils.toString(in));
-        } catch (IOException e) {
-            sb.append("Slang");
-        }
-        sb.append(System.lineSeparator());
-        sb.append(SlangCLI.getVersion());
+	@Override
+	public String getVersion() {
+		return SlangCLI.getVersion();
+	}
 
-        return sb.toString();
-    }
+	@Override
+	public String getWelcomeMessage() {
+		return ASSISTANCE;
+	}
 
-    @Override
-    public String getVersion() {
-        return SlangCLI.getVersion();
-    }
-
-    @Override
-    public String getWelcomeMessage() {
-        return assistance;
-    }
-
-    @Override
-    public String name() {
-        return "Slang-";
-    }
 }
--- score-lang-cli/src/main/java/com/hp/score/lang/cli/SlangPrompt.java
+++ score-lang-cli/src/main/java/com/hp/score/lang/cli/SlangPrompt.java
@@ -1,44 +1,27 @@
+/*
+ * Licensed to Hewlett-Packard Development Company, L.P. under one or more contributor license agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
 package com.hp.score.lang.cli;
 
-/*
- * Licensed to Hewlett-Packard Development Company, L.P. under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
-*/
-
-
+import org.springframework.core.annotation.Order;
 import org.springframework.shell.plugin.PromptProvider;
 import org.springframework.stereotype.Component;
 
 /**
  * Date: 11/7/2014
- *
  * @author lesant
  */
 @Component
-public class SlangPrompt implements PromptProvider {
-    private static final String prompt = "slang>";
-    private static final String name = "Slang";
-    @Override
-    public String getPrompt() {
-        return prompt;
-    }
+@Order(Integer.MIN_VALUE)
+public class SlangPrompt extends SlangNamedProvider implements PromptProvider {
 
-    @Override
-    public String name() {
-        return name;
-    }
+	@Override
+	public String getPrompt() {
+		return "slang>";
+	}
+
 }
