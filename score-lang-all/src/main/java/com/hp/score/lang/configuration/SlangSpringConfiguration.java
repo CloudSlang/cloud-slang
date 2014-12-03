652fae7d9ae73fa22c963d0e4efe4527e47e3f07
oritstone <orit.stone@hp.com>
2014-12-03 15:19:22 +0200

    Add new module - score-lang-all for packaging the slang runtime & compiler

---
+++ score-lang-all/src/main/java/com/hp/score/lang/configuration/SlangSpringConfiguration.java
@@ -0,0 +1,33 @@
+package com.hp.score.lang.configuration;
+/*
+ * Licensed to Hewlett-Packard Development Company, L.P. under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
+
+import com.hp.score.lang.compiler.configuration.SlangCompilerSpringConfig;
+import com.hp.score.lang.runtime.configuration.SlangRuntimeSpringConfig;
+import org.springframework.context.annotation.Import;
+
+/**
+ * User: stoneo
+ * Date: 03/12/2014
+ * Time: 10:39
+ */
+@Import({SlangRuntimeSpringConfig.class, SlangCompilerSpringConfig.class})
+public class SlangSpringConfiguration {
+
+}
