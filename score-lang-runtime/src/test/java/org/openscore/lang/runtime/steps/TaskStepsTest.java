1235f400e90948c34973ef7198ae39db2d2a096c
Meir Wahnon <meir.wahnon@hp.com>
2014-12-31 10:49:20 +0200

    align license headers

--- score-lang-runtime/src/test/java/org/openscore/lang/runtime/steps/TaskStepsTest.java
+++ score-lang-runtime/src/test/java/org/openscore/lang/runtime/steps/TaskStepsTest.java
@@ -1,10 +1,12 @@
-/*
- * Licensed to Hewlett-Packard Development Company, L.P. under one or more contributor license agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
- * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the
- * License.
- */
+/*******************************************************************************
+* (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License v2.0 which accompany this distribution.
+*
+* The Apache License is available at
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+*******************************************************************************/
 package org.openscore.lang.runtime.steps;
 
 import org.openscore.lang.entities.ResultNavigation;
@@ -50,16 +52,6 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-/*******************************************************************************
-* (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Apache License v2.0 which accompany this distribution.
-*
-* The Apache License is available at
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-*******************************************************************************/
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TaskStepsTest.Config.class)
