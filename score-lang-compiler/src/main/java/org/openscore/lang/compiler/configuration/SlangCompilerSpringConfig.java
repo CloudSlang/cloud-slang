e4003e3e2702635fc46c9fc2f857617f9a6f5352
Orit Stone <orit.stone@hp.com>
2015-02-08 11:07:14 +0200

    Remove unneeded bean definition
    Signed-off-by: Orit Stone <orit.stone@hp.com>

--- score-lang-compiler/src/main/java/org/openscore/lang/compiler/configuration/SlangCompilerSpringConfig.java
+++ score-lang-compiler/src/main/java/org/openscore/lang/compiler/configuration/SlangCompilerSpringConfig.java
@@ -13,10 +13,7 @@ package org.openscore.lang.compiler.configuration;
 /*
  * Created by orius123 on 05/11/14.
  */
-import org.openscore.lang.compiler.modeller.SlangModeller;
-import org.openscore.lang.compiler.modeller.SlangModellerImpl;
-import org.openscore.lang.compiler.scorecompiler.ScoreCompiler;
-import org.openscore.lang.compiler.scorecompiler.ScoreCompilerImpl;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -28,15 +25,9 @@ import org.yaml.snakeyaml.introspector.BeanAccess;
 public class SlangCompilerSpringConfig {
 
     @Bean
-    public Yaml yaml(){
+    public Yaml yaml() {
         Yaml yaml = new Yaml();
         yaml.setBeanAccess(BeanAccess.FIELD);
         return yaml;
     }
-
-    @Bean
-    public ScoreCompiler scoreCompiler(){
-        return new ScoreCompilerImpl();
-    }
-
 }
