fc93eebf72766d63ad6c6bb2dca2131919a8c834
Orit Stone <orit.stone@hp.com>
2015-02-16 09:35:36 +0200

    Add validation that argument passed is not empty and that it is a directory
    Signed-off-by: Orit Stone <orit.stone@hp.com>

--- score-lang-content-verifier/src/main/java/org/openscore/lang/tools/verifier/SlangFilesVerifier.java
+++ score-lang-content-verifier/src/main/java/org/openscore/lang/tools/verifier/SlangFilesVerifier.java
@@ -9,9 +9,12 @@
  *******************************************************************************/
 package org.openscore.lang.tools.verifier;
 
+import org.apache.commons.lang.Validate;
 import org.openscore.lang.tools.verifier.configuration.VerifierSpringConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.File;
 
 /*
  * Created by stoneo on 1/11/2015.
@@ -20,6 +23,9 @@ public class SlangFilesVerifier {
 
     public static void main(String[] args) {
         String repositoryPath = args[0];
+        Validate.notNull(repositoryPath, "You must pass a path to your repository");
+        Validate.isTrue(new File(repositoryPath).isDirectory(), "Directory path argument \'" + repositoryPath + "\' does not lead to a directory");
+
         ApplicationContext context = new AnnotationConfigApplicationContext(VerifierSpringConfiguration.class);
         VerifierHelper verifierHelper = context.getBean(VerifierHelper.class);
         try {
