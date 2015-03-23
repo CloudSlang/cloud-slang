e396b8a30767b9c26bde7eb01f054ebad1eda469
Eliya Sadan <eliyasadan@gmail.com>
2015-03-23 10:32:24 +0200

    remove import
    Signed-off-by: Eliya Sadan <eliyasadan@gmail.com>

--- score-lang-content-verifier/src/main/java/org/openscore/lang/tools/build/tester/parse/SlangTestCase.java
+++ score-lang-content-verifier/src/main/java/org/openscore/lang/tools/build/tester/parse/SlangTestCase.java
@@ -13,7 +13,6 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
