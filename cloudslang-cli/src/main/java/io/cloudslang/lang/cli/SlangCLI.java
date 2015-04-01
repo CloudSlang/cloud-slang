f835e353914c64a2bc063546e5ac11e73d117d61
Bonczidai Levente <levente.bonczidai@hp.com>
2015-04-01 13:54:22 +0300

    add events to CLI
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- cloudslang-cli/src/main/java/io/cloudslang/lang/cli/SlangCLI.java
+++ cloudslang-cli/src/main/java/io/cloudslang/lang/cli/SlangCLI.java
@@ -147,6 +147,8 @@ public class SlangCLI implements CommandMarker {
         handlerTypes.add(ScoreLangConstants.EVENT_INPUT_END);
         handlerTypes.add(ScoreLangConstants.EVENT_OUTPUT_START);
         handlerTypes.add(ScoreLangConstants.EVENT_OUTPUT_END);
+        handlerTypes.add(ScoreLangConstants.EVENT_BRANCH_START);
+        handlerTypes.add(ScoreLangConstants.EVENT_BRANCH_END);
         handlerTypes.add(ScoreLangConstants.SLANG_EXECUTION_EXCEPTION);
         handlerTypes.add(ScoreLangConstants.EVENT_EXECUTION_FINISHED);
         scoreServices.subscribe(new ScoreEventListener() {
