61ef8b70f4aa5fbe3d25cfb31d40a5c6e213dc65
Orit Stone <orit.stone@hp.com>
2015-04-19 19:42:30 +0300

    Fix bug in checking the output value - support also non string outputs
    Signed-off-by: Orit Stone <orit.stone@hp.com>

--- cloudslang-content-verifier/src/main/java/io/cloudslang/lang/tools/build/tester/SlangTestRunner.java
+++ cloudslang-content-verifier/src/main/java/io/cloudslang/lang/tools/build/tester/SlangTestRunner.java
@@ -223,7 +223,7 @@ public class SlangTestRunner {
                 String outputName = output.getKey();
                 Serializable outputValue = output.getValue();
                 Serializable executionOutputValue = executionOutputs.get(outputName);
-                if(!StringUtils.equals((String)executionOutputValue, (String)outputValue)){
+                if(!executionOutputValue.equals(outputValue.toString())){
                     message = TEST_CASE_FAILED + testCaseName + " - " + testCase.getDescription() + "\n\tFor output: " + outputName+ "\n\tExpected value: " + outputValue + "\n\tActual value: " + executionOutputValue;
                     log.error(message);
                     throw new RuntimeException(message);
