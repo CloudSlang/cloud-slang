22848e437965d72cee32d5b1e05e8175bd411c79
Eliya Sadan <eliyasadan@gmail.com>
2015-05-18 10:25:23 +0300

    header in the right place
    Signed-off-by: Eliya Sadan <eliyasadan@gmail.com>

--- build/build_content/upload_to_gh.sh
+++ build/build_content/upload_to_gh.sh
@@ -23,17 +23,15 @@ echo ${assets_url}
 
 echo -e "\n\nUpoloading artifacts using curl\n=================================================================================\n"
 
-auth_header="Authorization:token $ghusertoken"
-
 #echo -e "\n\nUpoloading slang-cli.tar.gz\n"
 #
 #curl  --insecure -XPOST -H "Authorization:token $ghusertoken" -H "Content-Type:application/gzip" --data-binary @slang-cli.tar.gz https://uploads.github.com/repos/$myrepo/releases/$uniquereleaseid/assets?name=slang-cli.tar.gz
 
 echo -e "\n\n$cli_location\n"
 
-curl  --insecure -XPOST -H ${auth_header} -H "Content-Type:application/zip" --data-binary @${cli_location} ${assets_url}?name=cslang-cli-with-content.zip
+curl  --insecure -XPOST -H "Authorization:token ${ghusertoken}" -H "Content-Type:application/zip" --data-binary @${cli_location} ${assets_url}?name=cslang-cli-with-content.zip
 
 #echo -e "\n\n$builder_location\n"
 
-#curl  --insecure -XPOST -H ${auth_header} -H "Content-Type:application/zip" --data-binary @${builder_location} ${assets_url}?name=cslang-builder.zip
+#curl  --insecure -XPOST -H "Authorization:token $ghusertoken" -H "Content-Type:application/zip" --data-binary @${builder_location} ${assets_url}?name=cslang-builder.zip
 
