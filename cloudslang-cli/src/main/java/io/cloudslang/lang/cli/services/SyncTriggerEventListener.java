3279b438b6ae4ba26486d7a4b676eefb61e88747
Eliya Sadan <eliyasadan@gmail.com>
2015-06-02 17:31:53 +0300

    fix cli printings
    Signed-off-by: Eliya Sadan <eliyasadan@gmail.com>

--- cloudslang-cli/src/main/java/io/cloudslang/lang/cli/services/SyncTriggerEventListener.java
+++ cloudslang-cli/src/main/java/io/cloudslang/lang/cli/services/SyncTriggerEventListener.java
@@ -73,9 +73,10 @@ public class SyncTriggerEventListener implements ScoreEventListener{
                 errorMessage.set(SLANG_STEP_ERROR_MSG + data.get(LanguageEventData.EXCEPTION));
                 break;
             case ScoreLangConstants.EVENT_INPUT_END:
-                String taskName = (String)data.get(LanguageEventData.STEP_NAME);
-                if(StringUtils.isNotEmpty(taskName)){
-                    String path = (String) data.get(LanguageEventData.PATH);
+                LanguageEventData eventData = (LanguageEventData) data;
+                if(eventData.getStepType() == LanguageEventData.StepType.TASK){
+                    String taskName = eventData.getStepName();
+                    String path = eventData.getPath();
                     int matches = StringUtils.countMatches(path, ExecutionPath.PATH_SEPARATOR);
                     String prefix = StringUtils.repeat(TASK_PATH_PREFIX, matches);
                     printWithColor(Ansi.Color.YELLOW, prefix + taskName);
