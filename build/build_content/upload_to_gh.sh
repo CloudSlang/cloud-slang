e93d153f29832bb2fd089eb546cb9c2da2c690e7
Eliya Sadan <eliyasadan@gmail.com>
2015-07-28 14:22:05 +0300

    Deploy gzip with content to GH
    Signed-off-by: Eliya Sadan <eliyasadan@gmail.com>

--- build/build_content/upload_to_gh.sh
+++ build/build_content/upload_to_gh.sh
@@ -6,7 +6,8 @@ BRANCH=$GIT_BRANCH
 BUILD_DIR=$WORKSPACE
 
 CLI_DIR=build/target/cloudslang-cli
-CLI_PATH=${CLI_DIR}/cslang-cli.zip
+CLI_ZIP_PATH=${CLI_DIR}/cslang-cli.zip
+CLI_GZIP_PATH=${CLI_DIR}/cslang-cli.tar.gz
 BUILDER_PATH=build/target/cslang-builder/cslang-builder.zip
 
 cd ${BUILD_DIR}
@@ -29,8 +30,11 @@ echo -e "\n\nUpoloading artifacts using curl\n==================================
 
 UPLOAD_URL="${RELEASES_URL}/$RELEASEID/assets"
 
-FILESIZE=`stat -c '%s' "$CLI_PATH"`
-curl -s -H "Authorization:token ${GHUSERTOKEN}" -H "Content-Type:application/zip" --data-binary "@${CLI_PATH}" "${UPLOAD_URL}?name=cslang-cli-with-content.zip&size=$FILESIZE"
+FILESIZE=`stat -c '%s' "$CLI_ZIP_PATH"`
+curl -s -H "Authorization:token ${GHUSERTOKEN}" -H "Content-Type:application/zip" --data-binary "@${CLI_ZIP_PATH}" "${UPLOAD_URL}?name=cslang-cli-with-content.zip&size=$FILESIZE"
+
+FILESIZE=`stat -c '%s' "$CLI_GZIP_PATH"`
+curl -s -H "Authorization:token ${GHUSERTOKEN}" -H "Content-Type:application/gzip" --data-binary "@${CLI_GZIP_PATH}" "${UPLOAD_URL}?name=cslang-cli-with-content.tar.gzip&size=$FILESIZE"
 
 echo -e "\n\nRemoving pre-release from release"
 curl -XPATCH -H "Authorization:token ${GHUSERTOKEN}" -d '{"prerelease": false}' 'https://api.github.com/repos/${REPO}/releases/$RELEASEID'
