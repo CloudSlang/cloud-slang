7b5b743833d45cd5616dc23b768b51a75944f2b5
gituser173 <claudiu-laurian.ban@hp.com>
2016-01-21 15:50:22 +0200

    Flow description - NullPointerException when section missing
     Signed-off-by: Claudiu Ban claudiu-laurian.ban@hpe.com

--- cloudslang-compiler/src/test/java/io/cloudslang/lang/compiler/MetadataExtractorTest.java
+++ cloudslang-compiler/src/test/java/io/cloudslang/lang/compiler/MetadataExtractorTest.java
@@ -25,6 +25,7 @@ public class MetadataExtractorTest {
                                                        "corresponding value addressed by the json_path input.\n";
     public static final String FIRST_INPUT_VALUE = "JSON data input \n" +
                                                    "- Example: '{\"k1\": {\"k2\": [\"v1\", \"v2\"]}}'\n";
+    public static final String FIRST_OUTPUT_VALUE = "the corresponding value of the key referred to by json_path";
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
@@ -33,7 +34,7 @@ public class MetadataExtractorTest {
 
     @Test
     public void testExtractMetadata() throws Exception {
-        URI operation = getClass().getResource("/metadata.sl").toURI();
+        URI operation = getClass().getResource("/metadata/metadata.sl").toURI();
         Metadata metadata = metadataExtractor.extractMetadata(SlangSource.fromFile(operation));
         Assert.assertNotNull("metadata is null", metadata);
         Assert.assertEquals("different description", OPERATION_DESCRIPTION, metadata.getDescription());
@@ -47,7 +48,7 @@ public class MetadataExtractorTest {
 
     @Test
     public void testExtractMetadataNoResults() throws Exception {
-        URI operation = getClass().getResource("/metadata_no_results.sl").toURI();
+        URI operation = getClass().getResource("/metadata/metadata_no_results.sl").toURI();
         Metadata metadata = metadataExtractor.extractMetadata(SlangSource.fromFile(operation));
         Assert.assertNotNull("metadata is null", metadata);
         Assert.assertEquals("different description", OPERATION_DESCRIPTION, metadata.getDescription());
@@ -60,8 +61,36 @@ public class MetadataExtractorTest {
     }
 
     @Test
+    public void testExtractMetadataNoOutputs() throws Exception {
+        URI operation = getClass().getResource("/metadata/metadata_no_outputs.sl").toURI();
+        Metadata metadata = metadataExtractor.extractMetadata(SlangSource.fromFile(operation));
+        Assert.assertNotNull("metadata is null", metadata);
+        Assert.assertEquals("different description", OPERATION_DESCRIPTION, metadata.getDescription());
+        Assert.assertEquals("different number of inputs", 2, metadata.getInputs().size());
+        Assert.assertEquals("different number of outputs", 0, metadata.getOutputs().size());
+        Assert.assertEquals("different number of results", 2, metadata.getResults().size());
+        String key = metadata.getInputs().keySet().iterator().next();
+        Assert.assertEquals("different input name", "json_input", key);
+        Assert.assertEquals("different input value", FIRST_INPUT_VALUE, metadata.getInputs().get(key));
+    }
+
+    @Test
+    public void testExtractMetadataNoInputs() throws Exception {
+        URI operation = getClass().getResource("/metadata/metadata_no_inputs.sl").toURI();
+        Metadata metadata = metadataExtractor.extractMetadata(SlangSource.fromFile(operation));
+        Assert.assertNotNull("metadata is null", metadata);
+        Assert.assertEquals("different description", OPERATION_DESCRIPTION, metadata.getDescription());
+        Assert.assertEquals("different number of inputs", 0, metadata.getInputs().size());
+        Assert.assertEquals("different number of outputs", 4, metadata.getOutputs().size());
+        Assert.assertEquals("different number of results", 2, metadata.getResults().size());
+        String key = metadata.getOutputs().keySet().iterator().next();
+        Assert.assertEquals("different input name", "value", key);
+        Assert.assertEquals("different input value", FIRST_OUTPUT_VALUE, metadata.getOutputs().get(key));
+    }
+
+    @Test
     public void testExtractMetadataWrongDescriptionFormat() throws Exception {
-        URI operation = getClass().getResource("/metadata_wrong_desc.sl").toURI();
+        URI operation = getClass().getResource("/metadata/metadata_wrong_desc.sl").toURI();
         exception.expect(RuntimeException.class);
         exception.expectMessage("could not found expected ':'");
         metadataExtractor.extractMetadata(SlangSource.fromFile(operation));
@@ -69,7 +98,7 @@ public class MetadataExtractorTest {
 
     @Test
     public void testExtractMetadataWrongDescriptionIndentation() throws Exception {
-        URI operation = getClass().getResource("/metadata_wrong_indentation.sl").toURI();
+        URI operation = getClass().getResource("/metadata/metadata_wrong_indentation.sl").toURI();
         exception.expect(RuntimeException.class);
         exception.expectMessage("expected <block end>, but found BlockSequenceStart");
         metadataExtractor.extractMetadata(SlangSource.fromFile(operation));
@@ -77,7 +106,7 @@ public class MetadataExtractorTest {
 
     @Test
     public void testExtractMetadataInvalidOutputValue() throws Exception {
-        URI operation = getClass().getResource("/metadata_invalid_output_value.sl").toURI();
+        URI operation = getClass().getResource("/metadata/metadata_invalid_output_value.sl").toURI();
         exception.expect(RuntimeException.class);
         exception.expectMessage("expected <block end>, but found Scalar");
         metadataExtractor.extractMetadata(SlangSource.fromFile(operation));
