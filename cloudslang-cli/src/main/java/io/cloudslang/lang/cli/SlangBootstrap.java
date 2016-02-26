ffac6f8859a045ac2b4470f97fe51e11528e52ec
Bonczidai Levente <levente.bonczidai@hpe.com>
2016-02-26 10:03:12 +0200

    add possibility of config file to CLI
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hpe.com>

--- cloudslang-cli/src/main/java/io/cloudslang/lang/cli/SlangBootstrap.java
+++ cloudslang-cli/src/main/java/io/cloudslang/lang/cli/SlangBootstrap.java
@@ -11,9 +11,17 @@
  */
 package io.cloudslang.lang.cli;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.shell.Bootstrap;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Bonczidai Levente
@@ -21,9 +29,64 @@ import java.io.IOException;
  */
 public class SlangBootstrap {
 
+    private static final String USER_CONFIG_DIR = "configuration";
+    private static final String USER_CONFIG_FILENAME = "cslang.properties";
+    private static final String USER_CONFIG_FILEPATH = USER_CONFIG_DIR + File.separator + USER_CONFIG_FILENAME;
+    private static final String SUBSTITUTION_REGEX = "\\$\\{([^${}]+)\\}"; // ${VAR}
+    private static final Pattern SUBSTITUTION_PATTERN = Pattern.compile(SUBSTITUTION_REGEX);
+    private static final String LOG4J_CONFIGURATION = "log4j.configuration";
+
     public static void main(String[] args) throws IOException {
+        Properties userProperties = loadUserProperties();
+        configureLogging(userProperties);
         System.out.println("Loading..");
         Bootstrap.main(args);
+    }
+
+    private static Properties loadUserProperties() throws IOException {
+        String appHome = System.getProperty("app.home");
+        String propertyFilePath = appHome + File.separator + USER_CONFIG_FILEPATH;
+        File propertyFile = new File(propertyFilePath);
+        Properties rawProperties = new Properties();
+        if (propertyFile.exists()) {
+            try (InputStream propertiesStream = new FileInputStream(propertyFilePath)) {
+                rawProperties.load(propertiesStream);
+            }
+        }
+        Properties processedProperties = new Properties();
+        Enumeration<?> e = rawProperties.propertyNames();
+        while (e.hasMoreElements()) {
+            String key = (String) e.nextElement();
+            String value = rawProperties.getProperty(key);
+            value = substitutePropertyReferences(value);
+            processedProperties.setProperty(key, value);
+        }
+        return processedProperties;
+    }
+
+    private static String substitutePropertyReferences(String value) {
+        Matcher mather = SUBSTITUTION_PATTERN.matcher(value);
+        Set<String> variableNames = new HashSet<>();
+        while (mather.find()) {
+            variableNames.add(mather.group(1));
+        }
+        for (String variableName : variableNames) {
+            String variableValue = System.getProperty(variableName);
+            if (StringUtils.isNotEmpty(variableValue)) {
+                value = value.replace("${" + variableName + "}", variableValue);
+            }
+        }
+        return value;
+    }
+
+    private static void configureLogging(Properties userProperties) {
+        String filePath = userProperties.getProperty(LOG4J_CONFIGURATION);
+        if (StringUtils.isNotEmpty(filePath)) {
+            // log4j will not search for config files in classpath resources
+            System.setProperty("log4j.configuration", filePath);
+            // load log4j properties from file
+            PropertyConfigurator.configure(filePath);
+        }
     }
 
 }
