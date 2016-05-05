70aa196a74ea45cda1558406c9b1fa060261eecf
Orit Stone <orit.stone@hp.com>
2016-05-05 09:48:06 +0300

    Fix java doc

--- cloudslang-compiler/src/main/java/io/cloudslang/lang/compiler/SlangCompiler.java
+++ cloudslang-compiler/src/main/java/io/cloudslang/lang/compiler/SlangCompiler.java
@@ -48,8 +48,8 @@ public interface SlangCompiler {
      * Validate that the given {@Link io.cloudslang.lang.compiler.modeller.model.Executable} is valid regarding
      * its wiring to its dependencies
      * Current validations:
-     *      - Validates that required inputs of the dependecy have a matching input in the task
-     *      - Validate that every result of teh dependency has a matching navigation in the task
+     *      - Validates that required inputs of the dependency have a matching input in the step
+     *      - Validate that every result of teh dependency has a matching navigation in the step
      * @param slangModel the CloudSlang model to validate
      * @param dependenciesModels the CloudSlang models of the direct dependencies
      * @return a list of the exceptions that were found (if any)
