ccbea9aff9e6b101f469435ed2175f38909ba1db
Genadi Rabinovich <genadi@hpe.com>
2016-05-08 11:34:05 +0300

    fixing spring context configuration

--- cloudslang-runtime/src/main/java/io/cloudslang/lang/runtime/configuration/SlangRuntimeSpringConfig.java
+++ cloudslang-runtime/src/main/java/io/cloudslang/lang/runtime/configuration/SlangRuntimeSpringConfig.java
@@ -17,9 +17,11 @@ import org.python.core.Options;
 import org.python.core.PySystemState;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Import;
+import io.cloudslang.runtime.impl.RuntimeManagementConfiguration;
 @Configuration
 @ComponentScan("io.cloudslang.lang.runtime")
+@Import({RuntimeManagementConfiguration.class})
 public class SlangRuntimeSpringConfig {
 
     static {
