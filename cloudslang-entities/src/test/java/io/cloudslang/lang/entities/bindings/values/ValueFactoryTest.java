84178a258a967bc21f580c7f535208fdecc6fd59
gituser173 <claudiu-laurian.ban@hp.com>
2016-06-27 17:08:15 +0300

    increase test coverage
    Signed-off-by: Claudiu Ban claudiu-laurian.ban@hpe.com

--- cloudslang-entities/src/test/java/io/cloudslang/lang/entities/bindings/values/ValueFactoryTest.java
+++ cloudslang-entities/src/test/java/io/cloudslang/lang/entities/bindings/values/ValueFactoryTest.java
@@ -7,7 +7,6 @@ import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
