f152c64bab2b2c3bdd4232735248bf2367b955cf
gituser173 <claudiu-laurian.ban@hp.com>
2016-07-19 13:45:04 +0300

    Input not required bug fix
    Signed-off-by: Claudiu Ban claudiu-laurian.ban@hpe.com

--- cloudslang-runtime/src/main/java/io/cloudslang/lang/runtime/bindings/InputsBinding.java
+++ cloudslang-runtime/src/main/java/io/cloudslang/lang/runtime/bindings/InputsBinding.java
@@ -98,12 +98,22 @@ public class InputsBinding {
                 scriptContext.putAll(targetContext);
                 value = scriptEvaluator.evalExpr(expressionToEvaluate, scriptContext, systemProperties, input.getFunctionDependencies());
                 value = ValueFactory.create(value, sensitive);
-            } else if (!isEmpty(rawValue)) {
+            } else if ((value == null && rawValue != null) || (containsEmptyStringOrNull(value) && doesNotContainNull(rawValue))) {
                 value = rawValue;
             }
         }
 
         return value;
+    }
+
+    private boolean containsEmptyStringOrNull(Value value) {
+        return value != null &&
+                ((value.get() != null && value.get().equals("")) ||
+                        value.get() == null);
+    }
+
+    private boolean doesNotContainNull(Value value) {
+        return value != null && value.get() != null;
     }
 
     private boolean isEmpty(Value value) {
