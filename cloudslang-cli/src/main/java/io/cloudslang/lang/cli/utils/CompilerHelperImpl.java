1b149f714ccfda70d605d6f51527720e500d7c3c
Lucian Musca <lucian-cristian.musca@hpe.com>
2016-10-13 13:14:21 +0300

    fixing cloudslang-cli Signed-off-by: Lucian Musca <lucian-cristian.musca@hpe.com>

--- cloudslang-cli/src/main/java/io/cloudslang/lang/cli/utils/CompilerHelperImpl.java
+++ cloudslang-cli/src/main/java/io/cloudslang/lang/cli/utils/CompilerHelperImpl.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * (c) Copyright 2016 Hewlett-Packard Development Company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *******************************************************************************/
 package io.cloudslang.lang.cli.utils;
 
 import ch.lambdaj.function.convert.Converter;
