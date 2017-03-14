d1e9bfdd8bd40050f406bc4ee11c1741b4e20ff0
Bonczidai Levente <levente.bonczidai@hpe.com>
2017-03-14 14:30:46 +0200

    modifiers step level - pre-compile
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hpe.com>

--- cloudslang-entities/src/main/java/io/cloudslang/lang/entities/bindings/Argument.java
+++ cloudslang-entities/src/main/java/io/cloudslang/lang/entities/bindings/Argument.java
@@ -34,6 +34,16 @@ public class Argument extends InOutParam {
         privateArgument = true;
     }
 
+    public Argument(
+            String name,
+            Value value,
+            boolean privateArgument,
+            Set<ScriptFunction> scriptFunctions,
+            Set<String> systemPropertyDependencies) {
+        super(name, value, scriptFunctions, systemPropertyDependencies);
+        this.privateArgument = privateArgument;
+    }
+
     public Argument(String name) {
         super(name, null);
         privateArgument = false;
