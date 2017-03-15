8a85de5bd5d36272286084ee59e365dd728d624f
Bonczidai Levente <levente.bonczidai@hpe.com>
2017-03-15 18:54:33 +0200

    tests - sensitivity - override, transitive cases
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hpe.com>

--- cloudslang-tests/src/test/java/io/cloudslang/lang/systemtests/sensitive/SensitiveValueSyntaxInFlowTest.java
+++ cloudslang-tests/src/test/java/io/cloudslang/lang/systemtests/sensitive/SensitiveValueSyntaxInFlowTest.java
@@ -15,6 +15,7 @@ import io.cloudslang.lang.entities.CompilationArtifact;
 import io.cloudslang.lang.systemtests.StepData;
 import io.cloudslang.lang.systemtests.ValueSyntaxParent;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import org.junit.Test;
@@ -72,6 +73,43 @@ public class SensitiveValueSyntaxInFlowTest extends ValueSyntaxParent {
         verifyInOutParams(stepData.getInputs());
         verifyInOutParams(stepData.getOutputs());
         verifySuccessResult(flowData);
+    }
+
+    @Test
+    public void testValuesStepInputModifiersSpecialCases() throws Exception {
+        // compile
+        URI resource = getClass().getResource("/yaml/sensitive/sensitive_values_flow_step.sl").toURI();
+        URI op1 = getClass().getResource("/yaml/noop.sl").toURI();
+        URI op2 = getClass().getResource("/yaml/print.sl").toURI();
+
+        Set<SlangSource> path = Sets.newHashSet(SlangSource.fromFile(op1), SlangSource.fromFile(op2));
+        CompilationArtifact compilationArtifact = slang.compile(SlangSource.fromFile(resource), path);
+
+        // trigger
+        Map<String, StepData> steps = prepareAndRun(compilationArtifact);
+
+        // verify
+        StepData flowData = steps.get(EXEC_START_PATH);
+        StepData stepData = steps.get(FIRST_STEP_PATH);
+
+        verifyParamsSensitivity(flowData.getInputs(), createExpectedFlowInputsSensitivity(), "flow inputs");
+        verifyParamsSensitivity(stepData.getInputs(), createExpectedStepInputsSensitivity(), "step inputs");
+
+        verifySuccessResult(flowData);
+    }
+
+    private Map<String, Boolean> createExpectedFlowInputsSensitivity() {
+        Map<String, Boolean> expectedFlowInputsSensitivity = new HashMap<>();
+        expectedFlowInputsSensitivity.put("input_no_value_modifier", false);
+        expectedFlowInputsSensitivity.put("input_transitivity", true);
+        return expectedFlowInputsSensitivity;
+    }
+
+    private Map<String, Boolean> createExpectedStepInputsSensitivity() {
+        Map<String, Boolean> expectedStepInputsSensitivity = new HashMap<>();
+        expectedStepInputsSensitivity.put("input_no_value_modifier", true);
+        expectedStepInputsSensitivity.put("input_transitivity", true);
+        return expectedStepInputsSensitivity;
     }
 
 }
