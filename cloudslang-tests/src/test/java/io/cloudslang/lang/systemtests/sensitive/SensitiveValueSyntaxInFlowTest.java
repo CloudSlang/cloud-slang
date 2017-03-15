1f863d4688f27c6c8e8e5a89739913eef2a77cd4
Bonczidai Levente <levente.bonczidai@hpe.com>
2017-03-15 15:28:53 +0200

    tests - sensitive data
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hpe.com>

--- cloudslang-tests/src/test/java/io/cloudslang/lang/systemtests/sensitive/SensitiveValueSyntaxInFlowTest.java
+++ cloudslang-tests/src/test/java/io/cloudslang/lang/systemtests/sensitive/SensitiveValueSyntaxInFlowTest.java
@@ -14,11 +14,9 @@ import io.cloudslang.lang.compiler.SlangSource;
 import io.cloudslang.lang.entities.CompilationArtifact;
 import io.cloudslang.lang.systemtests.StepData;
 import io.cloudslang.lang.systemtests.ValueSyntaxParent;
-
 import java.net.URI;
 import java.util.Map;
 import java.util.Set;
-
 import org.junit.Test;
 
 /**
@@ -51,4 +49,29 @@ public class SensitiveValueSyntaxInFlowTest extends ValueSyntaxParent {
         verifyInOutParams(stepData.getOutputs());
         verifySuccessResult(flowData);
     }
+
+    @Test
+    public void testValuesStepInputModifiers() throws Exception {
+        // compile
+        URI resource = getClass().getResource("/yaml/sensitive/sensitive_values_flow_step_input_modifiers.sl").toURI();
+        URI op1 = getClass().getResource("/yaml/noop.sl").toURI();
+        URI op2 = getClass().getResource("/yaml/print.sl").toURI();
+
+        Set<SlangSource> path = Sets.newHashSet(SlangSource.fromFile(op1), SlangSource.fromFile(op2));
+        CompilationArtifact compilationArtifact = slang.compile(SlangSource.fromFile(resource), path);
+
+        // trigger
+        Map<String, StepData> steps = prepareAndRun(compilationArtifact);
+
+        // verify
+        StepData flowData = steps.get(EXEC_START_PATH);
+        StepData stepData = steps.get(FIRST_STEP_PATH);
+
+        verifyInOutParams(flowData.getInputs());
+        verifyInOutParams(flowData.getOutputs());
+        verifyInOutParams(stepData.getInputs());
+        verifyInOutParams(stepData.getOutputs());
+        verifySuccessResult(flowData);
+    }
+
 }
