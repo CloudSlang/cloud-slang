1f52a1569eb2f45ffd0635dd0df2681dc18b9e5d
George Giloan <giloan@hpe.com>
2017-10-10 10:06:01 +0300

    Change the ExecutableBuilder component to handle do_external steps in cloudslang workflows.

--- cloudslang-compiler/src/test/java/io/cloudslang/lang/compiler/ExternalSlangCompilerIntegrationTest.java
+++ cloudslang-compiler/src/test/java/io/cloudslang/lang/compiler/ExternalSlangCompilerIntegrationTest.java
@@ -1,6 +1,6 @@
 package io.cloudslang.lang.compiler;
 
-import io.cloudslang.lang.compiler.modeller.model.Executable;
+import io.cloudslang.lang.compiler.modeller.model.Flow;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.io.File;
 import java.net.URL;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {io.cloudslang.lang.compiler.configuration.SlangCompilerSpringConfig.class})
 public class ExternalSlangCompilerIntegrationTest {
@@ -17,12 +20,12 @@ public class ExternalSlangCompilerIntegrationTest {
     private SlangCompiler slangCompiler;
 
     @Test
-    public void testExternalStepDraciLaciPLM() throws Exception {
+    public void testFlowWithExternalStep() throws Exception {
         final URL resource = getClass().getResource("/flow_with_external_steps.yaml");
         final SlangSource slangSource = SlangSource.fromFile(new File(resource.toURI()));
-        final Executable slangExecutable = slangCompiler.preCompile(slangSource);
+        final Flow slangExecutable = (Flow) slangCompiler.preCompile(slangSource);
 
-        System.out.println(slangExecutable.toString());
-
+        assertTrue(slangExecutable.getExternalExecutableDependencies().contains("/Library/Utility Operations/Flow Variable Manipulation/Do Nothing"));
+        assertEquals(slangExecutable.getWorkflow().getSteps(), 4);
     }
 }
