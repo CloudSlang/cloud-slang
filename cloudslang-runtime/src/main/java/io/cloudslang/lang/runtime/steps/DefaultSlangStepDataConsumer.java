0403b74dddcd40f12c10f67bb828fa97fbdd9e42
nitoiu <cristian.nitoiu@hpe.com>
2018-04-26 09:38:21 +0300

    add generic step data consumer

---
+++ cloudslang-runtime/src/main/java/io/cloudslang/lang/runtime/steps/DefaultSlangStepDataConsumer.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * (c) Copyright 2016 Hewlett-Packard Development Company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *******************************************************************************/
+package io.cloudslang.lang.runtime.steps;
+
+import io.cloudslang.lang.entities.bindings.values.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class DefaultSlangStepDataConsumer implements SlangStepDataConsumer {
+
+    @Override
+    public void consumeStepData(Map<String, Value> arguments, Map<String, Value> outputs) {
+
+    }
+}
