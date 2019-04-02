1bb812141e86d8579b3d831bbc481eba0abaddef
ionutGrad <ionut-adrian.grad@microfocus.com>
2019-04-02 14:18:34 +0300

    refactor
    Signed-off-by: ionutGrad <ionut-adrian.grad@microfocus.com>

--- cloudslang-compiler/src/main/java/io/cloudslang/lang/compiler/SlangTextualKeys.java
+++ cloudslang-compiler/src/main/java/io/cloudslang/lang/compiler/SlangTextualKeys.java
@@ -70,7 +70,6 @@ public interface SlangTextualKeys {
     String SEQ_STEP_ACTION_KEY = "action";
     String SEQ_STEP_ARGS_KEY = "args";
     String SEQ_STEP_DEFAULT_ARGS_KEY = "default_args";
-
     String SEQ_STEP_HIGHLIGHT_ID_KEY = "highlight_id";
     String SEQ_STEP_SNAPSHOT_KEY = "snapshot";
 
