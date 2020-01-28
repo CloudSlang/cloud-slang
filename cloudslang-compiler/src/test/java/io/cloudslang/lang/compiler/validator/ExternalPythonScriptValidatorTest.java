40ed482d10894c5aba5cf1b48d99bedc4baa9953
Alex Bajzat <alexandru.bajzat@microfocus.com>
2020-01-28 19:23:41 +0200

    Fixed check style and header

--- cloudslang-compiler/src/test/java/io/cloudslang/lang/compiler/validator/ExternalPythonScriptValidatorTest.java
+++ cloudslang-compiler/src/test/java/io/cloudslang/lang/compiler/validator/ExternalPythonScriptValidatorTest.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * (c) Copyright 2016 Hewlett-Packard Development Company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *******************************************************************************/
 package io.cloudslang.lang.compiler.validator;
 
 import org.junit.Rule;
@@ -9,9 +18,9 @@ import java.util.List;
 
 public class ExternalPythonScriptValidatorTest {
     private static String LINE_SEPARATOR = System.lineSeparator();
-    private static final String VALID_PYTHON_SCRIPT = "def execute({params}):" + LINE_SEPARATOR
-            + "  a = in1" + LINE_SEPARATOR
-            + "  return locals()" + LINE_SEPARATOR;
+    private static final String VALID_PYTHON_SCRIPT = "def execute({params}):" + LINE_SEPARATOR +
+            "  a = in1" + LINE_SEPARATOR +
+            "  return locals()" + LINE_SEPARATOR;
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
