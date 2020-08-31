7276d92d30856306852f55268cffaed9b3b761f1
MHorvat <maria.horvat@microfocus.com>
2020-08-31 15:37:46 +0300

    Update cs_extract_number function to retrieve float numbers too. Add extra parameter to cs_round function that decide the number of digits to be rounded.

--- cloudslang-runtime/src/main/resources/scripts/cs_round.py
+++ cloudslang-runtime/src/main/resources/scripts/cs_round.py
@@ -1,4 +1,5 @@
-def cs_round(val):
+def cs_round(val, digits = 0):
   x = float(val) if  type(val).__name__ == 'str' or  type(val).__name__ == 'unicode' else val
+  y = int(digits) if  type(val).__name__ == 'str' or  type(val).__name__ == 'unicode' else digits
 
-  return str(round(x))
+  return str(round(x, y))
