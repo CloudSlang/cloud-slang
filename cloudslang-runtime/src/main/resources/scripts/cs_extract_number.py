cf35d9f74a16bd8b4683a907087d6eb25b88a9c4
Larisa Bratean <larisa-alexandra.bratean@microfocus.com>
2020-11-17 13:08:36 +0200

    add import before def
    Signed-off-by: Larisa Bratean <larisa-alexandra.bratean@microfocus.com>

--- cloudslang-runtime/src/main/resources/scripts/cs_extract_number.py
+++ cloudslang-runtime/src/main/resources/scripts/cs_extract_number.py
@@ -1,5 +1,5 @@
+import re
 def cs_extract_number(string, count = 1):
-  import re
   if type(string).__name__ != 'str' and type(string).__name__ != 'unicode':
     raise Exception("Expected a string for parameter 'string', got " + str(string))
 
