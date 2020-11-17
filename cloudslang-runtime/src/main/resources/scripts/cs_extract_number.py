d1c6de7ad83cea92a9496a45a12aff1853afe286
Larisa Bratean <larisa-alexandra.bratean@microfocus.com>
2020-11-17 12:53:44 +0200

    import re because it's used for findall
    Signed-off-by: Larisa Bratean <larisa-alexandra.bratean@microfocus.com>

--- cloudslang-runtime/src/main/resources/scripts/cs_extract_number.py
+++ cloudslang-runtime/src/main/resources/scripts/cs_extract_number.py
@@ -1,4 +1,5 @@
 def cs_extract_number(string, count = 1):
+  import re
   if type(string).__name__ != 'str' and type(string).__name__ != 'unicode':
     raise Exception("Expected a string for parameter 'string', got " + str(string))
 
