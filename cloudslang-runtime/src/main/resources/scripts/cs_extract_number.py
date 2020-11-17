e990e1d7517d2604d34745ef90113a6bcb795a2e
larisabratean <36994740+larisabratean@users.noreply.github.com>
2020-11-17 13:49:33 +0200

    import re for jython (#1268)
    * import re because it's used for findall
    
    Signed-off-by: Larisa Bratean <larisa-alexandra.bratean@microfocus.com>
    
    * add import before def
    
    Signed-off-by: Larisa Bratean <larisa-alexandra.bratean@microfocus.com>
    
    Co-authored-by: Larisa Bratean <larisa-alexandra.bratean@microfocus.com>

--- cloudslang-runtime/src/main/resources/scripts/cs_extract_number.py
+++ cloudslang-runtime/src/main/resources/scripts/cs_extract_number.py
@@ -1,3 +1,4 @@
+import re
 def cs_extract_number(string, count = 1):
   if type(string).__name__ != 'str' and type(string).__name__ != 'unicode':
     raise Exception("Expected a string for parameter 'string', got " + str(string))
