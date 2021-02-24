7519e36101d5586d04a6d7d6465d07b4f5d7eeb8
Iuliana-Andreea-Totoian <iuliana-andreea.totoian@microfocus.com>
2021-02-24 14:17:57 +0200

    round number function returns integers

--- cloudslang-runtime/src/main/resources/scripts/cs_round.py
+++ cloudslang-runtime/src/main/resources/scripts/cs_round.py
@@ -2,7 +2,4 @@ def cs_round(val, digits = 0):
   x = float(val) if  type(val).__name__ == 'str' or  type(val).__name__ == 'unicode' else val
   y = int(digits) if  type(val).__name__ == 'str' or  type(val).__name__ == 'unicode' else digits
 
-  if y == 0:
-    return str(round(x))
-  else:
-    return str(round(x, y))
+  return str(round(x)) if y == 0 else str(round(x, y))
