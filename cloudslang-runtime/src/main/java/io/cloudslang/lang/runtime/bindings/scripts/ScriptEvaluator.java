b8afcc5f87cb438625a5e0bf277dffe65f1557ec
opreateodora <74269902+opreateodora@users.noreply.github.com>
2022-12-20 15:50:41 +0200

    Cases for evalExpr function (#1393)
    * add cases for evalExpr function
    
    * add cases for testExpr function
    
    Co-authored-by: TVerdesOprea <teodora.verdesoprea@microfocus.com>

--- cloudslang-runtime/src/main/java/io/cloudslang/lang/runtime/bindings/scripts/ScriptEvaluator.java
+++ cloudslang-runtime/src/main/java/io/cloudslang/lang/runtime/bindings/scripts/ScriptEvaluator.java
@@ -66,12 +66,9 @@ public class ScriptEvaluator extends ScriptProcessor {
                           Set<ScriptFunction> functionDependencies) {
         try {
             switch (PYTHON_EVALUATOR) {
-                //TODO add method for python server once the server is implemented; also copy for the default method
-                case PYTHON_SERVER:
-                case PYTHON:
-                    return doEvaluateExpressionExternalPython(expr, context, systemProperties, functionDependencies);
                 case JYTHON:
                     return doEvaluateExpressionJython(expr, context, systemProperties, functionDependencies);
+                //case PYTHON_SERVER or PYTHON
                 default:
                     return doEvaluateExpressionExternalPython(expr, context, systemProperties, functionDependencies);
             }
@@ -128,10 +125,6 @@ public class ScriptEvaluator extends ScriptProcessor {
                           Set<ScriptFunction> functionDependencies, long timeoutPeriod) {
         try {
             switch (PYTHON_EVALUATOR) {
-                //TODO add method for python server once the server is implemented; also copy for the default method
-                case PYTHON_SERVER:
-                case PYTHON:
-                    return doTestExternalPython(expr, context, systemProperties, functionDependencies, timeoutPeriod);
                 case JYTHON:
                     return doTestJython(expr, context, systemProperties, functionDependencies, timeoutPeriod);
                 default:
