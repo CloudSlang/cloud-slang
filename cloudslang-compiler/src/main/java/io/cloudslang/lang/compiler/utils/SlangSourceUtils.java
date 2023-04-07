7094596075717139308bec6b56e33c6d32257eca
Ashwin <ashwin.polla@microfocus.com>
2023-04-07 12:48:02 +0530

    Added list iterator parameters fix and worker group on simple loops fix

--- cloudslang-compiler/src/main/java/io/cloudslang/lang/compiler/utils/SlangSourceUtils.java
+++ cloudslang-compiler/src/main/java/io/cloudslang/lang/compiler/utils/SlangSourceUtils.java
@@ -10,11 +10,12 @@
 package io.cloudslang.lang.compiler.utils;
 
 import io.cloudslang.lang.compiler.SlangSource;
-import java.io.BufferedReader;
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
+import java.io.Reader;
 import java.io.Serializable;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,21 +27,11 @@ import static io.cloudslang.lang.compiler.SlangTextualKeys.NEXT_STEP;
  */
 public abstract class SlangSourceUtils {
     public static List<String> readLines(SlangSource source) {
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new StringReader(source.getContent()))) {
-            String nextLine = getNextLine(reader);
-            while (nextLine != null) {
-                lines.add(nextLine);
-                nextLine = reader.readLine();
-            }
+        try (Reader reader = new StringReader(source.getContent())) {
+            return IOUtils.readLines(reader);
         } catch (IOException e) {
             throw new RuntimeException("Error reading " + source.getName() + ":" + e.getMessage(), e);
         }
-        return lines;
-    }
-
-    private static String getNextLine(BufferedReader reader) throws IOException {
-        return reader.readLine();
     }
 
     public static String getNavigationStepName(Serializable value) {
