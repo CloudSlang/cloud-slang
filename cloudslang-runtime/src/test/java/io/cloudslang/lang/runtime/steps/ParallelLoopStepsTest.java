95ae4469066db561e7ec92c4640affeba4fe9cee
GIlca <gabriel.ilca@microfocus.com>
2023-09-06 12:10:47 +0300

    fix tests

--- cloudslang-runtime/src/test/java/io/cloudslang/lang/runtime/steps/ParallelLoopStepsTest.java
+++ cloudslang-runtime/src/test/java/io/cloudslang/lang/runtime/steps/ParallelLoopStepsTest.java
@@ -36,11 +36,13 @@ import io.cloudslang.runtime.api.python.executor.services.PythonExecutorConfigur
 import io.cloudslang.runtime.api.python.executor.services.PythonExecutorLifecycleManagerService;
 import io.cloudslang.runtime.api.python.PythonRuntimeService;
 import io.cloudslang.runtime.api.python.executor.entities.PythonExecutorDetails;
+import io.cloudslang.runtime.api.python.executor.services.PythonExecutorProcessManagerService;
 import io.cloudslang.runtime.impl.python.PythonExecutionCachedEngine;
 import io.cloudslang.runtime.impl.python.PythonExecutionEngine;
 import io.cloudslang.runtime.impl.python.PythonRuntimeServiceImpl;
 import io.cloudslang.runtime.impl.python.executor.services.ExternalPythonExecutorServiceImpl;
 import io.cloudslang.runtime.impl.python.executor.services.PythonExecutorLifecycleManagerServiceImpl;
+import io.cloudslang.runtime.impl.python.executor.services.PythonExecutorProcessManagerServiceImpl;
 import io.cloudslang.runtime.impl.python.external.ExternalPythonExecutionEngine;
 import io.cloudslang.score.api.EndBranchDataContainer;
 import io.cloudslang.score.events.EventBus;
@@ -623,7 +625,12 @@ public class ParallelLoopStepsTest {
         @Bean(name = "pythonExecutorLifecycleManagerService")
         public PythonExecutorLifecycleManagerService pythonExecutorLifecycleManagerService() {
             return new PythonExecutorLifecycleManagerServiceImpl(pythonExecutorCommunicationService(),
-                    pythonExecutorConfigurationDataService());
+                    pythonExecutorConfigurationDataService(), pythonExecutorProcessManagerService());
+        }
+
+        @Bean(name = "pythonExecutorProcessManagerService")
+        public PythonExecutorProcessManagerService pythonExecutorProcessManagerService() {
+            return mock(PythonExecutorProcessManagerServiceImpl.class);
         }
 
         @Bean(name = "externalPythonRuntimeService")
