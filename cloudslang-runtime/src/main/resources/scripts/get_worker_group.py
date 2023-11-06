feef84e70e67b703638fa01466fba7445d7014ad
GIlca <gabriel.ilca@microfocus.com>
2023-11-06 15:44:51 +0200

    fix missing key name

--- cloudslang-runtime/src/main/resources/scripts/get_worker_group.py
+++ cloudslang-runtime/src/main/resources/scripts/get_worker_group.py
@@ -2,5 +2,5 @@ def get_worker_group():
     try:
         value = get_from_smaller_context("get_worker_group()")
     except NameError:
-        value = globals().get(key)
+        value = globals().get("get_worker_group()")
     return "" if value is None else value
